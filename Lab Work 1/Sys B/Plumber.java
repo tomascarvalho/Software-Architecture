0198e29378f8403559b3fc87d5a26e24bc880166
Tomás Carvalho <tomcarvalhoc12@gmail.com>
2017-02-23 21:18:05 +0000

    Change in Diagram B

--- Lab Work 1/Sys B/Plumber.java
+++ Lab Work 1/Sys B/Plumber.java
@@ -46,12 +46,12 @@ public class Plumber
 		****************************************************************************/
 
 
-		SourceFilter            source = new SourceFilter();	// This is a source filter - see SourceFilterTemplate.java
+	SourceFilter            source = new SourceFilter();	// This is a source filter - see SourceFilterTemplate.java
     AltitudeFilter          altitudeFilter = new AltitudeFilter();
     TemperatureFilter       temperatureFilter = new TemperatureFilter();
-		PressureFilter          pressureFilter = new PressureFilter();	// This is a standard filter - see FilterTemplate.java
+	PressureFilter          pressureFilter = new PressureFilter();	// This is a standard filter - see FilterTemplate.java
     PressureWildpointFilter pressureWildpointFilter = new PressureWildpointFilter();
-		WildpointSinkFilter     wildpointSinkFilter = new WildpointSinkFilter();		// This is a sink filter - see SinkFilterTemplate.java
+	WildpointSinkFilter     wildpointSinkFilter = new WildpointSinkFilter();		// This is a sink filter - see SinkFilterTemplate.java
     SinkFilter              sinkFilter = new SinkFilter();
     MiddleFilter            middleFilter = new MiddleFilter();
 
